ea81939b70ea810b7ea3cfc3b10b3d51816027b3
Rj12-art <rashj40@gmail.com>
2022-10-23 20:38:18 +0000

    Initial commit

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask
+
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def Hello_Rash():
+    return "Hello, Dummy. What took you so long?!!!!" 
+
+
+if __name__== '__main__':
+    app.run(port=3000, debug=True)
